19ea804f0ebaa774b7a1a45eeec5e711264b14e5
Cullen Brown <cjb3768@gmail.com>
2016-07-19 15:55:46 -0500

    Data recording, need to add downloading
    Records timestamps and timegaps, reports time gaps in an alert at end; does not correctly display updates to css, need to solve.

--- test.js
+++ test.js
@@ -7,19 +7,27 @@
 
 var testActive = false; //whether test is running
 var awaitingUser = false; //whether test is waiting on user to click
-var startTime, endTime;
-var testDuration = 60000;
-var waitDuration = 0;
+var startTime, finishTime; 
+var testDuration = 30000; //max duration of test in ms
+var waitDuration = 0; //timestamp for end of waiting period between tests
 var timeGaps = []; //user delay in clicking
-var timeDelay = 0;
+var startTimes = [];
+var endTimes = [];
+var avgGap=0; //average user delayin clicking
 var rightNow = Date.now();
+var timeDelay = 0; //current 
 var minDelay = 1000; //minimum delay between tests
 var maxDelay = 5000; //maximum delay between tests
     
 function calcTestTimeDelay(minTime,maxTime){
     return Math.floor(Math.random() * maxTime) + minTime;
 }
-    
+
+function storeTimestamps(start,end){
+    startTimes.push(start);
+    endTimes.push(end);
+}
+
 function calcInterval(start, end){
     return end - start;
 }
@@ -28,28 +36,40 @@ function storeInterval(start, end){
     timeGaps.push(calcInterval(start,end));
 }
 
-function getEndTime(timeLimit){
+function getFinishTime(timeLimit){
     return Date.now() + timeLimit;
 }
     
 function mouseDown(evt){
     if (testActive === false){
         //start test
+        //setWaitIndicators();
         testActive = true;
-        setWaitIndicators();
+        
+        for (var i = 0; i < 5; i++){
+            
+        }
         //$('#result').html("Beginning test, please wait");
         startPVT();
     }
     else{
         //test running
         //testActive = false;
-        $('#result').html("Test is running");
+        //$('#result').html("Test is running");
         if (!awaitingUser){
+            //alert user of false start (TO-DO: find out if these need to be isolated)
             $('#result').html("You've clicked too soon!");
         }
         else{
+            //store intervbal between delay and mouse down
+            var capturedClick = Date.now();
+            storeTimestamps(waitDuration, capturedClick);
+            storeInterval(waitDuration, capturedClick);
+            //reset indicators and flag
             setWaitIndicators();
             awaitingUser = false;
+            setWaitIndicators();
+            //set up next delay
             waitDelay();
         }
     }
@@ -58,12 +78,14 @@ function mouseDown(evt){
 function startPVT(){
     //clear existing data points
     timeGaps.length = 0;
+    startTimes.length = 0;
+    endTimes.length = 0;
     
     //get startTime
     startTime = Date.now();
     
-    //get endTime
-    endTime = getEndTime(testDuration);
+    //get finishTime
+    finishTime = getFinishTime(testDuration);
     
     //call function to wait for first duration
     waitDelay();
@@ -71,24 +93,25 @@ function startPVT(){
 
 function keepTesting(){
     rightNow = Date.now();
-    //see if we already passed the endTime
-    if (rightNow < endTime){
+    //see if we already passed the finishTime
+    if (rightNow < finishTime){
         //calculate delay and see if we can keep testing
         timeDelay = calcTestTimeDelay(minDelay,maxDelay);
         waitDuration = rightNow + timeDelay;
-        if ((waitDuration) < endTime){
+        if ((waitDuration) < finishTime){
             return true;
         }
         //not enough time for testing, end testing
         return false;
     }
     else{
-        //passed endTime, end testing
+        //passed finishTime, end testing
         return false;
     }
 }
 
 function waitDelay(){
+
     //see if test should be run
     if (keepTesting()){
         while (Date.now() < waitDuration){
@@ -99,16 +122,28 @@ function waitDelay(){
     }
     else{
         //handle ending
-        setCompleteIndicators();
-        testActive = false;
+        endPVT();
+        
     }
 }
 
+function endPVT(){
+    //end testing
+    testActive = false;
+    setCompleteIndicators();
+    alert("Testing complete! That test collected " + timeGaps.length + " timegaps");
+    var recordedTime = "";
+    for (var j = 0; j < timeGaps.length;j++){
+        recordedTime += "" + timeGaps[j] + "ms, ";
+    }
+    alert(recordedTime);
+}
 
 function setWaitIndicators(){
-    $("#page").css('backgroundColor','#aa0000');
-    $('#result').html("Click registered! Please wait.");
-    $("#start").html("Please wait");
+    $("#page").css('backgroundColor','#aa0000').show();
+    $('#result').html("Click registered! Please wait.").show();
+    //$("#start").html("Please wait").show();
+    
 }
 
 function setClickIndicators(){
@@ -120,6 +155,7 @@ function setCompleteIndicators(){
     $("#page").css('backgroundColor','#00eeff');
     $("#result").html("Click to start new test");
 }
+/**
     function runTest(timeLimit, pageID, buttonID){
         // run test for time limit (in ms)
         
@@ -168,6 +204,7 @@ function setCompleteIndicators(){
         }
         alert(recordedTime);
     }
+*/
 
 function init(){
     document.getElementById("page").onmousedown = mouseDown;
